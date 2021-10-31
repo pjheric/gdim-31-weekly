0c33b7f93c252cfe47a21bfa410a6b8b8715988e
pjheric <pjheric1219@gmail.com>
2021-10-30 23:37:04 -0700

    confiner
    added cinemachine confiner (collision issue not fixed)

--- GDIM 31 Weekly/Assets/Scripts/PlayerMovement.cs
+++ GDIM 31 Weekly/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     Animator playerAnimator; 
     void Start()
     {
+        //Initializing components
         rb = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
         bc = GetComponent<BoxCollider2D>(); 
@@ -21,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        //Retrieve run and flipping direction every frame. Check for jump separately
         Run();
         FlipSprite();
     }
@@ -30,6 +32,7 @@ public class PlayerMovement : MonoBehaviour
     }
     void OnJump(InputValue value)
     {
+        //Instead of raycast, use a simple collision detection with Ground layer to determine if player is on a platform
         if (!bc.IsTouchingLayers(LayerMask.GetMask("Ground")))
             return; 
         if(value.isPressed)
@@ -41,6 +44,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y); 
         rb.velocity = playerVelocity;
+        //Epsilon is there specifically to prevent the player sprite from always facing right when standing still
         bool playerMoving = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
         playerAnimator.SetBool("isWalking", playerMoving); 
     }
